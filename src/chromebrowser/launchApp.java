323596395694ec6ed0e408431f3dc5a97cdfa971
Punit Nanda <punitnanda2@gmail.com>
2020-07-09 18:55:26 +0530

    testing

---
+++ src/chromebrowser/launchApp.java
@@ -0,0 +1,31 @@
+package chromebrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class launchApp {
+	public WebDriver driver ;
+
+	@BeforeClass
+	public void createChromeDriver() {
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriversetup\\chromedriver_win32\\chromedriver.exe");
+		driver =new ChromeDriver();
+	}
+	
+	@Test
+	public void openBrowser() {
+		driver.get("http://www.google.com");
+		System.out.println("Title is" +driver.getTitle());
+		
+	}
+	
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+	
+}
